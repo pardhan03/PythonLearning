c26aa518523df51895203a68abb4a482d2b6bf4d
Manish Pardhan <manish@instaastro.in>
2026-09-16 09:01:37 +0530

    exit condition

---
+++ basics/grade_calculator.py
@@ -0,0 +1,17 @@
+score = 958
+
+if score >= 101:
+    print('Invalid grade')
+    exit()
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("D")
+
+
